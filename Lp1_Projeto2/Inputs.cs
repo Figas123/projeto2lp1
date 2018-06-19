97e885bd6165640ba0c39c0caf9a9aa3adac31c1
TN-10 <you@example.com>
2018-06-19 01:19:32 +0100

    Added more inputs

--- Lp1_Projeto2/Inputs.cs
+++ Lp1_Projeto2/Inputs.cs
@@ -11,11 +11,11 @@ namespace Lp1_Projeto2
         public void WhatInputs()
         {
             Console.WriteLine("\n Inputs:\n -----------");
-            Console.WriteLine("\n  (w) - Move NORTH");
-            Console.WriteLine("  (a) - Move WEST");
-            Console.WriteLine("  (d) - Move EAST");
-            Console.WriteLine("  (s) - Move SOUTH");
-            Console.WriteLine("  (q) - Quit Game");
+            Console.WriteLine("\n  (w) - Move NORTH  (a) - Move WEST     " +
+                "(d) - Move EAST  (s) - Move SOUTH");
+            Console.WriteLine("\n  (f) - Attack NPC  (e) - Pick up item  " +
+                "(u) - Use item   (v) - Drop item");
+            Console.WriteLine("\n  (i) - Information (q) - Quit game");
             Console.WriteLine("\n -----------");
         }
     }
